a0ab087bd5cdf49c886b4490f3e0c25abe56be6b
dakotasmith52 <dakotasmith52@github.com>
2018-07-19 22:21:03 -0700

    working transcription

--- transcribe.py
+++ transcribe.py
@@ -1,1 +1,13 @@
-import speech_recognition
+import speech_recognition as sr
+import boto3
+
+s3 = boto3.resource('s3')
+r = sr.Recognizer()
+
+def transcribe():
+    recording = sr.AudioFile('me.wav')
+    with recording as source:
+        audio = r.record(source)
+    print(r.recognize_google(audio))
+
+
